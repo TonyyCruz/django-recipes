952a41155799d075cd10393c4fcd4551bc645cfa
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-20 17:52:32 -0300

    feat: implementa parcialmente a interacao da pagina com o conteudo dinamico implementei uma fincao mock para gerar dados aleatorios para testar a passagem de dados para as receitas atraves da view e passei os dados mocados para testar na aplicacao

--- calories_counter/views.py
+++ calories_counter/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+from calories_counter.utils.calories_counter.factory import make_recipe
+
 
 def recipe_list(request):
-    return render(request, "calories_counter/pages/recipe_list.html")
+    return render(
+        request,
+        "calories_counter/pages/recipe_list.html",
+        context={
+            "recipes": [make_recipe() for _ in range(10)]
+        })
 
 
 def recipe_details(request, id):
-    return render(request, "calories_counter/pages/recipe_detail.html")
+    return render(
+        request,
+        "calories_counter/pages/recipe_detail.html",
+        context={
+            "recipe": make_recipe()
+        })
