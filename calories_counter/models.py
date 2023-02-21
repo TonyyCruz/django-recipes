0eac8a4fc8a84613ed0c3a576ed94e17d70a3dca
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-21 14:15:04 -0300

    feat and fix: altera um metodo do model que estava errado e faz a migracao

--- calories_counter/models.py
+++ calories_counter/models.py
@@ -1,13 +1,19 @@
+# TEMPORARIO
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
-    preparation_time = models.models.IntegerField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
-    servings = models.models.IntegerField()
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
@@ -15,3 +21,10 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to="calories_counter/covers/%Y/%m/%d")
+
+    Category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
