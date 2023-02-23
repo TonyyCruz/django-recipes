8c3c3a3341bb7ba4bf9b4d8f697ac7dd6778a6b7
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-23 16:30:26 -0300

    refactor: altera o nome do diretorio de templates dentro de templetes havia um diretorio chamado calories_counter seu nome foi alterado para recipes e os arquivos que o utilizavam fortam ajustados

--- calories_counter/models.py
+++ calories_counter/models.py
@@ -23,7 +23,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to="calories_counter/covers/%Y/%m/%d")
+    cover = models.ImageField(
+        upload_to="calories_counter/recipes/covers/%Y/%m/%d")
 
     category = models.ForeignKey(
         Category, on_delete=models.SET_DEFAULT, default="sem categoria"
