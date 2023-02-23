91833e3f6437ca8a966149ef4bf144c82b9368b6
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-23 14:58:34 -0300

    feat: adiciona erro 404 not found para detalhe da receita

--- calories_counter/views.py
+++ calories_counter/views.py
@@ -22,7 +22,7 @@ def recipe_list(request):
 
 
 def recipe_details(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = get_object_or_404(Recipe, id=id)
     return render(
         request,
         "calories_counter/pages/recipe_details.html",
