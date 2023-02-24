6f4c688fdd4e2cec0dcc791768e97b1b7222e587
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-24 16:31:35 -0300

    test: adiciona test para validar o template da pagina home

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -20,3 +20,11 @@ class RecipeViewsTest(TestCase):
     def test_recipe_view_category_is_correct(self):
         view = resolve(reverse("recipes:category", kwargs={"id": 1}))
         self.assertIs(views.category, view.func)
+
+    def test_recipe_home_view_return_status_code_200(self):
+        response = self.client.get(reverse("recipes:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse("recipes:home"))
+        self.assertTemplateUsed(response, "recipes/pages/home.html")
