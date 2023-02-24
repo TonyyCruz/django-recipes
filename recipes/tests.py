c9790233f3ef13f81367331538c966b2418fd3c7
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-24 02:12:23 -0300

    test: configuracoes iniciais para fazer os testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_home_page(self):
+        assert 1 == 1, "Esse texto sera exibido em caso de erro"
