b32555f6a4d75f78ddb0580b71407f5b264c1124
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-24 16:16:20 -0300

    refactor: cria um diretorio para organizar os tests os testes ja existentes foram movidos para arquivos independentes dessa forma os testes ficam mais organizados e o codigo fica mais limpo

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,27 +4,6 @@ from django.urls import resolve, reverse
 from recipes import views
 
 
-class RecipeURLsTest(TestCase):
-    # def test_fake(self):
-    #     assert 1 == 1, "Esse texto sera exibido em caso de erro"
-
-    def test_recipe_home_url(self):
-        url = reverse("recipes:home")
-        self.assertEqual(url, "/")
-
-    def test_recipe_list_url(self):
-        url = reverse("recipes:list")
-        self.assertEqual(url, "/recipes/")
-
-    def test_recipe_details_url(self):
-        url = reverse("recipes:details", kwargs={"id": 1})
-        self.assertEqual(url, "/recipe/1/")
-
-    def test_recipe_category_url(self):
-        url = reverse("recipes:category", kwargs={"id": 1})
-        self.assertEqual(url, "/recipe/category/1/")
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_view_home_is_correct(self):
         view = resolve(reverse("recipes:home"))
