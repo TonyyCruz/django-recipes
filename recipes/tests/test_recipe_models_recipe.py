d88bb448c389ebf9ef3ff84bf02393b87ea01478
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-25 15:41:06 -0300

    test: testa inserir titulo de receita com mais de 65 caracteres verifica se levanta um erro ao inserir uma quantidade invalida de caracteres

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -1,3 +1,5 @@
+from django.forms import ValidationError
+
 from .test_testcase_with_recipe import TestCaseWithRecipe
 
 
@@ -5,3 +7,10 @@ class RecipeModelTest(TestCaseWithRecipe):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def test_recipe_title_raise_error_if_title_bigger_than_65_characters(self):
+        title_with_100_characters = "A" * 100
+        with self.assertRaises(ValidationError):
+            self.recipe.title = title_with_100_characters
+            self.recipe.full_clean()  # valida os campos
+            self.recipe.save()
