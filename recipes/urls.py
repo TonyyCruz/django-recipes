cefb3419220c2542fdc0253671c47f5c5ad00f0e
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-26 22:07:53 -0300

    test: altera o nome de alguns testes para melhorar o entendimento

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,8 +7,8 @@ app_name = "recipes"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("recipes/search/", views.search, name="search"),
     path("recipes/", views.recipe_list, name="list"),
     path("recipe/<int:id>/", views.recipe_details, name="details"),
     path("recipes/category/<int:id>/", views.category, name="category"),
-    path("recipes/search/<str:q>/", views.search, name="search"),
 ]
