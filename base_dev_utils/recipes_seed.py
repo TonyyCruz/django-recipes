e6e311c226f199db67f9d0be607414cde672fb6b
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-02-28 20:41:13 -0300

    test: testa se a pagina search esta funcionando corretamente

---
+++ base_dev_utils/recipes_seed.py
@@ -0,0 +1,56 @@
+recipes = [
+    {
+        "title": "Arroz com calabresa na pressão fácil",
+        "description": str(
+            """
+            O arroz com calabresa na pressão fácil é superprático,
+            experimente servir com uma salada verde ou com batata frita,
+            fica uma delícia. Adicione azeitonas ou picles para um sabor
+            mais intenso."""
+        ),
+        "slug": "arroz-com-calabresa",
+        "preparation_time": 25,
+        "preparation_time_unit": "minutos",
+        "servings": 5,
+        "servings_unit": "porções",
+        "preparation_steps": str(
+            """
+                ingredientes
+
+                1 colher de sopa de óleo
+
+                1/2 cebola picada
+
+                1 gomo de linguiça calabresa cortada em fatias
+
+                3 dentes de alho picados
+
+                2 xícaras de chá de arroz
+
+                1 cenoura ralada
+
+                1 lata de milho escorrido
+
+                Cheiro-verde a gosto
+
+                1/2 sachê de molho de tomate
+
+                Sal a gosto
+
+                3 xícaras de chá de água quente
+                modo de preparo
+                Em uma panela de pressão, esquente o óleo e refogue a cebola,
+                a linguiça e o alho.
+                Adicione o arroz, a cenoura, o milho, o cheiro-verde e misture bem.
+                Acrescente o molho de tomate, o sal, a água quente, misture bem, tampe a panela e deixe pegar pressão no fogo alto.
+                Assim que pegar pressão, diminua o fogo, conte 3 minutos, desligue o fogo e deixe a pressão sair sozinha.
+                Agora é só servir. Bom apetite.
+            """
+        ),
+        "preparation_steps_is_html": False,
+        "is_published": True,
+        "cover": "base_images/covers/No-Image-Placeholder.svg.png",
+        "category": 1,
+        "author": 1,
+    },
+]
--- recipes/tests/test_search_views_recipes.py
+++ recipes/tests/test_search_views_recipes.py
@@ -28,3 +28,38 @@ class RecipesSearchViews(TestCaseWithRecipe):
     def test_recipe_search_if_raise_404_if_search_term_is_a_space(self):
         response = self.client.get(reverse("recipes:search") + "?q=   ")
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title_1 = "Farofa de batata com queijo"
+        title_2 = "Lasanha de carne com queijo"
+        recipe_1 = self.make_recipe(
+            title=title_1,
+            slug="farofa",
+            author=self.make_author(username="user1")
+        )
+        recipe_2 = self.make_recipe(
+            title=title_2,
+            slug="lasanha",
+            author=self.make_author(username="user2")
+        )
+
+        search_url = reverse("recipes:search")
+        response_1 = self.client.get(f"{search_url}?q=farofa")
+        response_2 = self.client.get(f"{search_url}?q=lasanha")
+        response_both = self.client.get(f"{search_url}?q=queijo")
+
+        self.assertIn(recipe_1, response_1.context["recipes"])
+        self.assertNotIn(recipe_1, response_2.context["recipes"])
+
+        self.assertIn(recipe_2, response_2.context["recipes"])
+        self.assertNotIn(recipe_2, response_1.context["recipes"])
+
+        self.assertIn(recipe_1, response_both.context["recipes"])
+        self.assertIn(recipe_2, response_both.context["recipes"])
+
+    def test_recipe_search_not_show_no_published_recipes(self):
+        recipe = self.make_recipe(is_published=False)
+        search_url = reverse("recipes:search")
+        response = self.client.get(f"{search_url}?q=something")
+
+        self.assertNotIn(recipe, response.context["recipes"])
