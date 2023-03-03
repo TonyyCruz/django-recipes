d1e7d970c55385cc5788eb9554adc3f80741eceb
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-03-03 18:11:21 -0300

    text: adiciona teste para a paginacao na lista de receitas

--- recipes/tests/test_recipe_list_view_recipes.py
+++ recipes/tests/test_recipe_list_view_recipes.py
@@ -34,3 +34,42 @@ class RecipesListViews(TestCaseWithRecipe):
         content = response.content.decode('utf-8')
 
         self.assertIn('Não há nenhuma receita ainda', content)
+
+    def test_recipe_list_have_a_correct_pagination_range(self):
+        """slow"""
+        self.make_multiples_recipes(72)
+        response = self.client.get(reverse("recipes:list") + "?page=1")
+        response_2 = self.client.get(reverse("recipes:list") + "?page=3")
+        response_3 = self.client.get(reverse("recipes:list") + "?page=4")
+
+        context = response.context["pagination_range"]
+        context_2 = response_2.context["pagination_range"]
+        context_3 = response_3.context["pagination_range"]
+
+        content = response.content.decode('utf-8')
+        content_2 = response_2.content.decode('utf-8')
+        content_3 = response_3.content.decode('utf-8')
+
+        self.assertEqual(context["page_range"], range(1, 7))
+        self.assertEqual(context["current_page"], 1)
+        self.assertEqual(context["stop_range"], 5)
+        self.assertIn(
+            "<spam class=\"page-link page-item current-page\">1</spam>",
+            content
+        )
+
+        self.assertEqual(context_2["page_range"], range(1, 7))
+        self.assertEqual(context_2["current_page"], 3)
+        self.assertEqual(context_2["stop_range"], 5)
+        self.assertIn(
+            "<spam class=\"page-link page-item current-page\">3</spam>",
+            content_2
+        )
+
+        self.assertEqual(context_3["page_range"], range(1, 7))
+        self.assertEqual(context_3["current_page"], 4)
+        self.assertEqual(context_3["stop_range"], 6)
+        self.assertIn(
+            "<spam class=\"page-link page-item current-page\">4</spam>",
+            content_3
+        )
