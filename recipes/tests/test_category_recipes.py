d9d8bbcc960571af79ffe22d871a28b461819071
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-03-04 19:11:45 -0300

    test refactor: adiciona um mock nos testes de paginacao

--- recipes/tests/test_category_recipes.py
+++ recipes/tests/test_category_recipes.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import resolve, reverse
 
 from recipes import views
@@ -24,45 +26,50 @@ class RecipesCategoryViews(TestCaseWithRecipe):
     def test_recipe_category_have_a_correct_pagination_range(self):
         """slow"""
         category = self.make_category(name="test_category")
-        self.make_multiples_recipes(72, category=category)
-        response = self.client.get(
-            reverse("recipes:category", kwargs={"id": category.id}) + "?page=1"
-        )
-        response_2 = self.client.get(
-            reverse("recipes:category", kwargs={"id": category.id}) + "?page=3"
-        )
-        response_3 = self.client.get(
-            reverse("recipes:category", kwargs={"id": category.id}) + "?page=4"
-        )
+        self.make_multiples_recipes(18, category=category)
 
-        context = response.context["pagination_range"]
-        context_2 = response_2.context["pagination_range"]
-        context_3 = response_3.context["pagination_range"]
+        with patch("recipes.views.ITEMS_PER_PAGE", new=3):
+            response = self.client.get(
+                reverse("recipes:category", kwargs={
+                        "id": category.id}) + "?page=1"
+            )
+            response_2 = self.client.get(
+                reverse("recipes:category", kwargs={
+                        "id": category.id}) + "?page=3"
+            )
+            response_3 = self.client.get(
+                reverse("recipes:category", kwargs={
+                        "id": category.id}) + "?page=4"
+            )
 
-        content = response.content.decode('utf-8')
-        content_2 = response_2.content.decode('utf-8')
-        content_3 = response_3.content.decode('utf-8')
+            context = response.context["pagination_range"]
+            context_2 = response_2.context["pagination_range"]
+            context_3 = response_3.context["pagination_range"]
 
-        self.assertEqual(context["page_range"], range(1, 7))
-        self.assertEqual(context["current_page"], 1)
-        self.assertEqual(context["stop_range"], 5)
-        self.assertIn(
-            "<spam class=\"page-link page-item current-page\">1</spam>",
-            content
-        )
+            content = response.content.decode('utf-8')
+            content_2 = response_2.content.decode('utf-8')
+            content_3 = response_3.content.decode('utf-8')
 
-        self.assertEqual(context_2["page_range"], range(1, 7))
-        self.assertEqual(context_2["current_page"], 3)
-        self.assertEqual(context_2["stop_range"], 5)
-        self.assertIn(
-            "<spam class=\"page-link page-item current-page\">3</spam>",
-            content_2
-        )
+            self.assertEqual(context["page_range"], range(1, 7))
+            self.assertEqual(context["current_page"], 1)
+            self.assertEqual(context["stop_range"], 5)
+            self.assertIn(
+                "<spam class=\"page-link page-item current-page\">1</spam>",
+                content
+            )
 
-        self.assertEqual(context_3["page_range"], range(1, 7))
-        self.assertEqual(context_3["current_page"], 4)
-        self.assertEqual(context_3["stop_range"], 6)
-        self.assertIn(
-            "<spam class=\"page-link page-item current-page\">4</spam>",
-            content_3
-        )
+            self.assertEqual(context_2["page_range"], range(1, 7))
+            self.assertEqual(context_2["current_page"], 3)
+            self.assertEqual(context_2["stop_range"], 5)
+            self.assertIn(
+                "<spam class=\"page-link page-item current-page\">3</spam>",
+                content_2
+            )
+
+            self.assertEqual(context_3["page_range"], range(1, 7))
+            self.assertEqual(context_3["current_page"], 4)
+            self.assertEqual(context_3["stop_range"], 6)
+            self.assertIn(
+                "<spam class=\"page-link page-item current-page\">4</spam>",
+                content_3
+            )
