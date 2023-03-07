92ce7248c11866d89b0820acc7b91152644d1cc2
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-03-07 00:46:59 -0300

    feat: adiciona mensagem de erro paca a confirmacao de password

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(forms.ModelForm):
+    confirm_password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Confirme sua senha",
+        }),
+        label="Confirmar senha"
+    )
+
     class Meta:
         model = User
         fields = [
@@ -24,3 +33,17 @@ class RegisterForm(forms.ModelForm):
                 "placeholder": "Insira sua senha aqui"
             })
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            error_message = 'Os campos "senha" e "confirmar senha" precisam ser iguais'  # noqa: E501
+            raise ValidationError({
+                "password": error_message,
+                "confirm_password": error_message,
+            },
+                code="invalid"
+            )
