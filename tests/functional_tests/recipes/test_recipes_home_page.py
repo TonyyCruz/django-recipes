ab878098c2b965bf11f04e474136ef5bf76f7ca6
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-03-16 16:09:59 -0300

    chore: adiciona "__init__.py" nos diretorios de test e markador foi adicionado o marcador "functional_test" para delimitar os testes que serao executados

--- tests/functional_tests/recipes/test_recipes_home_page.py
+++ tests/functional_tests/recipes/test_recipes_home_page.py
@@ -1,10 +1,11 @@
 
+import pytest
 from selenium.webdriver.common.by import By
 
-from tests.functional_tests.recipes.recipe_base_live_server_test import \
-    RecipeBaseFunctionalTest
+from .base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipesHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipes_home_without_recipes_shows_correct_message(self):
         self.browser.get(self.live_server_url)
