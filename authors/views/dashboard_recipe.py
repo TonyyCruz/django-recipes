ecc6b3944f04626b7815f8cfdb897a9afac8f2a8
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-04-22 17:34:56 -0300

    feat: adiciona decorator de verificacao de login se certifica que para criar e editar receitas precisa estar logado

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -1,13 +1,19 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 
 from authors.forms.recipe_form import RecipeForm
 from recipes.models import Recipe
 
 
+@method_decorator(
+    login_required(login_url="authors:login", redirect_field_name="next"),
+    name="dispatch",
+)
 class DashboardRecipe(View):
     def get_recipe(self, id, author):
         if id is None:
@@ -24,12 +30,11 @@ class DashboardRecipe(View):
 
         return recipe
 
-    def render_recipe(self, form, page_title="Recipe edit"):
+    def render_recipe(self, form):
         return render(
             self.request,
             "authors/pages/dashboard_recipe.html",
             context={
-                "page_title": page_title,
                 "form": form,
             },
         )
