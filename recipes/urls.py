61dcc22297edba0e1317fd8d609e8ba770ff92f8
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-04-24 17:27:01 -0300

    feat: adiciona classe base na rota recipes cria uma classe que sera usada de base para paginas que contenham listas de receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,8 +6,8 @@ app_name = "recipes"
 
 
 urlpatterns = [
+    path("", views.RecipeListViewBase.as_view(), name="home"),
     path("recipes/search/", views.search, name="search"),
-    path("", views.home, name="home"),
     path("recipe/<int:id>/", views.recipe_details, name="details"),
     path("recipes/category/<int:id>/", views.category, name="category"),
 ]
