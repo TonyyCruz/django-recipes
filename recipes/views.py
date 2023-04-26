49553348726b69a41c84cff3e27105fb17e2af45
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-04-26 17:46:55 -0300

    refactor: altera a view de detalhes de funcao para classe

--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,7 @@ import os
 
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from utils.pagination import make_pagination
 
@@ -91,74 +90,7 @@ class RecipeViewSearch(RecipeListViewBase):
         return context
 
 
-def recipe_details(request, id):
-    recipe = get_object_or_404(
-        Recipe.objects.filter(
-            id=id,
-            is_published=True,
-        )
-    )
-    return render(
-        request,
-        "recipes/pages/recipe_details.html",
-        context={
-            "recipe": recipe,
-        },
-    )
-
-
-# def category(request, id):
-#     category = get_object_or_404(Category, id=id)
-#     recipes = get_list_or_404(
-#         Recipe.objects.filter(is_published=True, category__id=id).order_by(
-#             "-id"
-#         )
-#     )
-#     pages_obj, pagination_range = make_pagination(
-#         request,
-#         recipes,
-#         ITEMS_PER_PAGE,
-#         qty_pages=QTY_PAGES_IN_PAGINATION,
-#     )
-
-#     return render(
-#         request,
-#         "recipes/pages/category.html",
-#         context={
-#             "pagination_range": pagination_range,
-#             "recipes": pages_obj,
-#             "is_recipe_list": True,
-#             "category": category,
-#         },
-#     )
-
-
-# def search(request):
-#     search_therm = request.GET.get("q", "").strip()
-#     recipes = Recipe.objects.filter(
-#         Q(title__icontains=search_therm)
-#         | Q(description__icontains=search_therm),
-#         is_published=True,
-#     ).order_by("-id")
-
-#     if not search_therm:
-#         raise Http404()
-
-#     pages_obj, pagination_range = make_pagination(
-#         request,
-#         recipes,
-#         ITEMS_PER_PAGE,
-#         qty_pages=QTY_PAGES_IN_PAGINATION,
-#     )
-
-#     return render(
-#         request,
-#         "recipes/pages/search.html",
-#         context={
-#             "pagination_range": pagination_range,
-#             "recipes": pages_obj,
-#             "page_title": f'Search: "{search_therm}"',
-#             "is_recipe_list": True,
-#             "additional_url_query": f"&q={search_therm}",
-#         },
-#     )
+class RecipeViewDetail(DetailView):
+    model = Recipe
+    context_object_name = "recipe"
+    template_name = "recipes/pages/recipe_details.html"
