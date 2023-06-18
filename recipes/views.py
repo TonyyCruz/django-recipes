ac5185e6aa027aec30e0d1a59d4e6894674e6cb6
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-18 14:13:50 -0300

    feat: exemplos de query

--- recipes/views.py
+++ recipes/views.py
@@ -15,7 +15,9 @@ QTY_PAGES_IN_PAGINATION = int(os.environ.get("QTY_PAGES_IN_PAGINATION", 5))
 
 
 def theory(request, *args, **kwargs):
-    context = {}
+    recipes = Recipe.objects.all()[:10]
+    simple_recipes = Recipe.objects.values("id", "title")[:10]
+    context = {"recipes": recipes, "simple_recipes": simple_recipes}
     return render(request, "recipes/pages/theory.html", context=context)
 
 
