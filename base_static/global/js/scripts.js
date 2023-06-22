1896604ae3c148dd8423855e8968240a9d8aef42
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-22 16:17:15 -0300

    feat: adiciona script em JS para menu foi adicionado script para exibir/esconder menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,15 +1,15 @@
-function myScope() {
-  const form = document.querySelectorAll(".form-delete");
-  const recipes = document.querySelectorAll(".recipe-name");
+(() => {
+  const form = document.querySelectorAll('.form-delete');
+  const recipes = document.querySelectorAll('.recipe-name');
 
   if (form) {
     form.forEach((formField, id) => {
-      formField.addEventListener("submit", (e) => {
+      formField.addEventListener('submit', (e) => {
         e.preventDefault();
         recipeName = recipes[id].innerHTML
         deleteText = `Do you really want to delete this recipe? ${recipeName}`;
 
-        const confirmed = confirm(deleteText)
+        const confirmed = confirm(deleteText);
 
         if (confirmed) {
           formField.submit();
@@ -17,6 +17,37 @@ function myScope() {
       });
     });
   };
-};
+})();
 
-myScope();
+
+(() => {
+  const buttonCloseMenu = document.querySelector('.button-close-menu');
+  const buttonShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
+  const displayNoneClass = 'hidden-text';
+
+  const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    buttonShowMenu.classList.add(displayNoneClass);
+    menuContainer.classList.remove(menuHiddenClass);
+  };
+
+  const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    buttonShowMenu.classList.remove(displayNoneClass);
+    menuContainer.classList.add(menuHiddenClass);
+  };
+
+  if (buttonShowMenu) {
+    buttonShowMenu.removeEventListener('click', showMenu);
+    buttonShowMenu.addEventListener('click', showMenu);
+  };
+
+  if (buttonCloseMenu) {
+    buttonCloseMenu.removeEventListener('click', closeMenu);
+    buttonCloseMenu.addEventListener('click', closeMenu);
+  };
+})();
