f242be44e3afe2fe42bb799fec4955d6773b1364
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-23 20:25:43 -0300

    revert: retorna para o comit antes da traducao commit numero: da681fdfe441fb5b6ebd7062c9b7f4ee16464786

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,8 +1,6 @@
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-from . import BASE_DIR
-
 LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "America/Sao_Paulo"
@@ -10,7 +8,3 @@ TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 
 USE_TZ = True
-
-LOCALE_PATHS = [
-    BASE_DIR / "locale",
-]
