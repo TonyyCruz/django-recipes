54fb2d1c2c299d39fd34d5c81bb25cbf01d07bd1
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-25 19:34:10 -0300

    feat: adiciona campos "category", "author", "tag" e "tag_object" ao RecipeSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -9,6 +18,18 @@ class RecipeSerializer(serializers.Serializer):
     # sejam diferentes
     public = serializers.BooleanField(source="is_published")
     preparation = serializers.SerializerMethodField()
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+    )
+    tag = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    tag_objects = TagSerializer(
+        many=True,
+        source="tag",
+    )
 
     # informa para "preparation" qual sera seu valor.
     # (atravez do SerializerMethodField), adicionamos get_<field>
