7da4a6ed63b5528ab326f0e6f4adea72596c9ce6
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-26 16:43:08 -0300

    refactor: altera os serializers Os serializer estava extendendo de serializers.Serializer tornando o codigo mais verboso, agora esta extendendo de serializers.ModelSerializer

--- recipes/serializers/recipes_serializer.py
+++ recipes/serializers/recipes_serializer.py
@@ -1,36 +1,50 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from tag.models import Tag
-
+from ..models import Recipe
 from .tag_serializer import TagSerializer
 
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            "id",
+            "title",
+            "description",
+            "public",
+            "preparation",
+            "category",
+            "author",
+            "tag",
+            "tag_objects",
+            "tag_links",
+        ]
+
     # Utilizamos "source" para indicar o campo origem, caso os nomes dos campo
     # sejam diferentes
-    public = serializers.BooleanField(source="is_published")
-    preparation = serializers.SerializerMethodField()
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(),
+    public = serializers.BooleanField(
+        source="is_published",
+        read_only=True,
     )
-    tag = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True,
+    # SerializerMethodField informa que o valor sera adquirido de um methodo
+    preparation = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    # StringRelatedField exibe o valor do campo no lugar do "id"
+    category = serializers.StringRelatedField(
+        read_only=True,
     )
     tag_objects = TagSerializer(
         many=True,
         source="tag",
+        read_only=True,
     )
+    # HyperlinkedRelatedField serve para inserir um link no campo
     tag_links = serializers.HyperlinkedRelatedField(
         source="tag",
         many=True,
-        queryset=Tag.objects.all(),
         view_name="recipes:recipes_tag_api_v2",
+        read_only=True,
     )
 
     # informa para "preparation" qual sera seu valor.
