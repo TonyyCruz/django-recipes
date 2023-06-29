6ee84fa236eea09aa21713925cdf5948dcb7f9ef
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-28 21:28:41 -0300

    fix: validacao do titulo feit no model foi retirada as receitas sendo usadas na aplicacao foram feitas automaticamente com o mesmo titulo e a validacao estava dando erro na aplicacao

--- recipes/models.py
+++ recipes/models.py
@@ -123,15 +123,16 @@ class Recipe(models.Model):
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
 
-        recipe_from_db = Recipe.objects.filter(
-            title__iexact=self.title
-        ).first()
+        # FOI COMENTADO POIS RECIPES CRIADAS ANTERIORMENTE ESTAVAM DANDO ERRO
+        # recipe_from_db = Recipe.objects.filter(
+        #     title__iexact=self.title
+        # ).first()
 
-        if recipe_from_db:
-            if recipe_from_db.pk != self.pk:
-                error_messages["title"].append(
-                    "Found recipes with the same title"
-                )
+        # if recipe_from_db:
+        #     if recipe_from_db.pk != self.pk:
+        #         error_messages["title"].append(
+        #             "Found recipes with the same title"
+        #         )
 
         if error_messages:
             raise ValidationError(error_messages)
