8bcf98a6fa4782c2bbe50f3e1b326f9b7e99a5ed
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-06-29 19:38:26 -0300

    refactor: refatora "RecipeAPIv2Details" para extender de "RetrieveUpdateDestroyAPIView"

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from tag.models import Tag
 
@@ -22,45 +24,9 @@ class RecipeAPIv2List(ListCreateAPIView):
     pagination_class = RecipeAPIv2Pagination
 
 
-class RecipeAPIv2Details(APIView):
-    def get_recipe(self, pk):
-        return get_object_or_404(
-            Recipe.objects.get_published(),
-            pk=pk,
-        )
-
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            many=False,
-            context={
-                "request": request,
-            },
-        )
-        return Response(serializer.data)
-
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            many=False,
-            data=request.data,
-            partial=True,
-            context={
-                "request": request,
-            },
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class RecipeAPIv2Details(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
 
 
 @api_view(http_method_names=["get", "patch", "delete"])
