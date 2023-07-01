c1e43d0d54132829adb2444b9a82ab36d14f9e17
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-07-01 19:23:53 -0300

    feat: implementa controle de permissao de usuario garante que apenas o dono da receita pode alterar ou deletar a mesma

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
 from ..models import Recipe
+from ..permissions import isOwner
 from ..serializers import RecipeSerializer, TagSerializer
 
 
@@ -18,6 +20,31 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self):
+        pk = self.kwargs.get("pk", "")
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(request=self.request, obj=obj)
+
+        return obj
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category_id = self.request.query_params.get("category_id", "")
+
+        if category_id != "" and category_id.isnumeric():
+            qs.filter(category_id=category_id)
+        return qs
+
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [isOwner()]
+        return super().get_permissions()
 
 
 @api_view()
