9ff428b78a503980b969c964d34d443c9e2e8df6
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-07-03 15:47:21 -0300

    refactor: altera o comportamento do validador de password

--- utils/django_form.py
+++ utils/django_form.py
@@ -1,17 +1,29 @@
 import re
 
-from django.forms import ValidationError
-
 
 def strong_password(password):
-    regex = re.compile(
-        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-    )
-    if not regex.match(password):
-        raise ValidationError((
-            "Password must have at least one uppercase letter, "
-            "one lowercase letter one number and one special character. "
-            "The length should be at least 8 characters."
-        ),
-            code="Invalid"
-        )
+    errors = list()
+    regex_lower_case = re.compile(r"(?=.*[a-z])")
+    regex_upper_case = re.compile(r"(?=.*?[A-Z])")
+    regex_one_number = re.compile(r"(?=.*?[0-9])")
+    regex_special_character = re.compile(r"(?=.*[@$!%*?&-(),.~#;:])")
+
+    if not password:
+        errors.append("Password must not be empty")
+
+    if len(password) < 8:
+        errors.append("Password must have at least 8 characters")
+
+    if not regex_upper_case.match(password):
+        errors.append("Password must have at least one uppercase letter")
+
+    if not regex_lower_case.match(password):
+        errors.append("Password must have at least one lowercase letter")
+
+    if not regex_one_number.match(password):
+        errors.append("Password must have at least one number")
+
+    if not regex_special_character.match(password):
+        errors.append("Password must have at least one special character.")
+
+    return errors
