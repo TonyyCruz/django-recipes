04a5b1ac941d523da8ee7bf2ee5c9c8bfcd48345
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-07-04 18:03:13 -0300

    fix: o serializer de author foi modificado para validar o "confirm password" no cadastro de usuario

--- authors/serializers/author_serializer.py
+++ authors/serializers/author_serializer.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from authors.validators import AuthorValidator
@@ -6,7 +6,7 @@ from authors.validators import AuthorValidator
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = [
             "id",
             "first_name",
@@ -15,12 +15,23 @@ class AuthorSerializer(serializers.ModelSerializer):
             "email",
             "password",
         ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
 
     def validate(self, attrs):
+        data = dict(attrs)
+        confirm_password = self.initial_data.get("confirm_password", "")
+        if confirm_password:
+            data["confirm_password"] = confirm_password
+
         AuthorValidator(
-            data=attrs,
+            data=data,
             ErrorClass=serializers.ValidationError,
             partial_update=self.partial,
             fields=self.fields,
         )
         return super().validate(attrs)
+
+    def create(self, validated_data):
+        return super().create(validated_data)
