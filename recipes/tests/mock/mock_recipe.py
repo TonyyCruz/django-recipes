c7189188afdd7cc597d26cac121ef877cab9880e
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-07-20 16:59:23 -0300

    fix test: ajusta o mock utilizado no teste

--- recipes/tests/mock/mock_recipe.py
+++ recipes/tests/mock/mock_recipe.py
@@ -2,9 +2,9 @@ mock_recipe = {
     "title": "Mock Recipe",
     "description": "My Description",
     "preparation_time": 10,
-    "preparation_time_unit": "min",
+    "preparation_time_unit": "minute",
     "servings": 10,
-    "servings_unit": "portions",
+    "servings_unit": "portion",
     "preparation_steps": """mock recipe mock recipe mock recipe mock recipe
     mock recipe mock recipe mock recipe mock recipe mock recipe mock recipe
     mock recipe mock recipe mock recipe mock recipe mock recipe mock recipe
